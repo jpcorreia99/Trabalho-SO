8f9282e15a977510e332c87e6a73e469a10bdabd
jpcorreia99 <jpcorreia99@gmail.com>
2020-05-24 13:58:43 +0100

    finish separate commands

--- src/main.c
+++ src/main.c
@@ -114,38 +114,6 @@ int main(int argc, char* argv[]){
     }
 
     if(strcmp("-e",argv[1])==0){
-        /*char *str = argv[2];
-        char str2[]= "grep -v ^# /etc/passwd | cut -f7 -d: | uniq | wc";
-        char str3[] = "pstree | wc";
-
-        char *** command_matrix = NULL;
-        char * tokenized_string = strtok(str, " ");
-        int n_args=0;  // contará em cada comando o número de argumentos
-        int n_commands = 0; // nº de comandos
-
-        int* size_commands; // array que guarda o número de parametros de cada comando
-        command_matrix = malloc(sizeof(char**));
-        while(tokenized_string){
-            if(strcmp(tokenized_string,"|")!=0){
-            command_matrix[n_commands] = realloc(command_matrix[n_commands], sizeof(char*) * ++n_args); // aloca espaço para mais um comando
-
-            command_matrix[n_commands][n_args-1] = tokenized_string;
-            }else{
-                command_matrix[n_commands] = realloc(command_matrix[n_commands], sizeof(char*) * ++n_args); // aloca espaço para mais um comando
-                command_matrix[n_commands][n_args-1] = NULL; // teermina a lista de strings de um comandos em null
-                size_commands = realloc(size_commands, sizeof(int) * (n_commands+1));
-                size_commands[n_commands++] = n_args;
-
-                command_matrix = realloc(command_matrix, sizeof(char**)*(n_commands+1)); // aloca espaço para mais uma linha de comandos
-                n_args=0;
-            }
-            tokenized_string = strtok(NULL, " ");
-        }
-        command_matrix[n_commands] = realloc(command_matrix[n_commands], sizeof(char*) * ++n_args); // aloca espaço para mais um comando
-        command_matrix[n_commands][n_args-1] = NULL; // teermina a lista de strings de um comandos em null
-        size_commands = realloc(size_commands, sizeof(int) * (n_commands+1));
-        size_commands[n_commands++] = n_args;*/
-
         int number_commands;
         int* size_commands_array;
         char*** command_matrix = separate_commands(argv,&number_commands,&size_commands_array);
@@ -161,7 +129,7 @@ int main(int argc, char* argv[]){
                 printf("%d %d %s//\n",i,j,command_matrix[i][j]);
             }
         }
-        //execute_pipe(command_matrix,number_commands,size_commands_array);
+        execute_pipe(command_matrix,number_commands,size_commands_array);
 
     }
     wait(NULL);
